0f7b73e52744efa589d7fff876e030b1598b6f56
lanebo1 <k.efimovich@innopolis.university>
2024-02-05 11:54:02 +0300

    Added support for PostgresSQL, updated requirements.txt

--- FixRemont/settings.py
+++ FixRemont/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
+from django.db.backends.postgresql.psycopg_any import IsolationLevel
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +27,7 @@ SECRET_KEY = 'django-insecure-ak3m^usi@ex9j!4rj8b@69!y7m@=0v7(_n*gj(3f3m!98r4hv0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -78,9 +80,14 @@ WSGI_APPLICATION = 'FixRemont.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'fixremontweb',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+    },
+    "OPTIONS": {
+        "isolation_level": IsolationLevel.SERIALIZABLE,
+    },
 }
 
 
@@ -118,7 +125,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
